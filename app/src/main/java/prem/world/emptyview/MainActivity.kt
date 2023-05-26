787146fd30ac4fc4b91a5f07bb04566642385e8f
PARIMAL PARMAR <ASHISHPARMAR4@LIVE.COM>
2023-05-26 17:13:33 +0530

    Initial commit

---
+++ app/src/main/java/prem/world/emptyview/MainActivity.kt
@@ -0,0 +1,40 @@
+package prem.world.emptyview
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rolButton: Button = findViewById(R.id.button)
+        rolButton.setOnClickListener {
+           rollDice()
+         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()//.toString()
+        val img: ImageView = findViewById(R.id.imageView)
+       val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        img.setImageResource(drawableResource)
+        img.contentDescription=(diceRoll.toString())
+        //return diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
