ed022af82e9ecf6407591b41a28d856a4d7f0687
PARIMAL PARMAR <ASHISHPARMAR4@LIVE.COM>
2023-05-28 12:30:36 +0530

    Added Unit Test

--- app/src/test/java/prem/world/emptyview/ExampleUnitTest.kt
+++ app/src/test/java/prem/world/emptyview/ExampleUnitTest.kt
@@ -1,17 +1,21 @@
 package prem.world.emptyview
 
+import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(5, 2 + 3)
     }
+
+    @Test
+    fun generateNumber() {
+        val dice = Dice(6)
+        val diceResult= dice.roll()
+        Assert.assertTrue("The value of rollResult was not between 1 and 6", diceResult in 1..6)
+    }
+
 }
